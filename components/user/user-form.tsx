6ff6d6fb2481096f50ee773b27a96e1961dc3628
bagoesrex <bagoesrex@gmail.com>
2025-07-10 02:03:43 +0700

    style: adjust padding and layout on user form container

--- components/user/user-form.tsx
+++ components/user/user-form.tsx
@@ -2,7 +2,7 @@ import UserFormField from "./user-form-field";
 
 export default function UserForm() {
     return (
-        <div className="bg-white/70 backdrop-blur-3xl py-4 px-4 rounded-md mx-30 border-b-2 border-x-2 border-purple-600">
+        <div className="bg-white/70 backdrop-blur-3xl py-4 px-4 rounded-md mx-6 border-b-2 border-x-2 border-purple-600">
             <h1 className="text-2xl font-bold mb-6 text-center text-purple-700">Tambah User Baru</h1>
             <UserFormField />
         </div>
